b6c7b5f7fe70ce97c2cfee273c72ab6e5153ca8b
Sergii <s.ignatov92@gmail.com>
2026-02-25 21:44:40 +0200

    Initial commit

---
+++ src/main/java/ee/subjecta/subjecta_backend/SubjectaBackendApplication.java
@@ -0,0 +1,13 @@
+package ee.subjecta.subjecta_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubjectaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubjectaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/subjecta/subjecta_backend/SubjectaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.subjecta.subjecta_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubjectaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
